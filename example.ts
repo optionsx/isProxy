d8a2e9e5a39202fa356c78a712bcc06dd9abae5c
optionsx <fake@protonmail.com>
2023-02-28 08:12:48 +0300

    q

--- example.ts
+++ example.ts
@@ -1,4 +1,4 @@
-import { isProxy } from "https://deno.land/x/isproxy/mod.ts";
+import isProxy from "https://deno.land/x/isproxy/mod.ts";
 const iipp = await isProxy("1.1.1.1");
 
 console.log(iipp);
